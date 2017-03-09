6899ef9174f28e01d245ec181926a2cf945b850d
kcharles <kcharles@a2plcpnl0901.prod.iad2.secureserver.net>
2017-03-09 10:50:11 -0700

    Database access set up

---
+++ db.php
@@ -0,0 +1,10 @@
+<?PHP
+include('../secure_passwords.php');
+$db = new mysqli($db_ip,$db_username,$db_password,"scaffo");
+
+// check connection
+if ($db->connect_errno) {
+    printf("Connect failed: %s\n", $db->connect_error);
+    exit();
+}
+?>
